55ee0ce7860a65125d90e696c96be42141b05942
Yi Yang <emeryyi@gmail.com>
2014-09-26 01:51:34 -0500

    1. first commite

---
+++ R/cv.mlda.R
@@ -0,0 +1,40 @@
+# cross validation
+cv.mlda <- function(x, y, nfolds = 10, lambda.opt = "min", ...) {
+    y <- drop(y)
+    n <- nrow(x)
+    p <- ncol(x)
+    ### Fit the model once to get dimensions etc of output
+    tmp <- mlda(x, y)
+    prior <- tmp$prior
+    lambda <- tmp$lambda
+    nlambda <- length(lambda)
+    ### Now fit the nfold models and store them
+    foldid <- sample(rep(seq(nfolds), length = n))
+    if (nfolds < 3) 
+        stop("nfolds must be bigger than 3; nfolds=10 recommended")
+    if (nfolds > n) 
+        stop("The number of folds should be smaller than the sample size.")
+    residmat <- matrix(0, nlambda, nfolds)
+    for (i in seq(nfolds)) {
+        which <- foldid == i
+        fit <- mlda(x[!which, , drop = FALSE], y[!which], lambda = lambda)
+        mu <- fit$mu
+        theta <- list2array(fit$theta)
+        pred <- predict.mlda(x[which, ], mu, theta, prior)
+        for (l in 1:nlambda) {
+            residmat[l, i] <- mean(y[which] != pred[, l])
+        }
+    }
+    residmat[is.na(residmat)] <- 0.5
+    residmat <- matrix(residmat, nrow = nlambda)
+    cv <- apply(residmat, 1, mean)
+    cv.error <- sqrt(apply(residmat, 1, var)/K)
+    if (lambda.opt == "min") {
+        bestlambda <- max(lambda[which(cv == min(cv))])
+    } else {
+        bestlambda <- max(lambda[which(cv == min(cv))])
+    }
+    obj <- list(lambda = lambda, cv = cv, cv.error = cv.error, bestlambda = bestlambda)
+    class(obj) <- "cv.mlda"
+    obj
+} 
---
+++ R/predict.mlda.R
@@ -0,0 +1,14 @@
+# predict routine
+predict.mlda <- function(x, mu, theta, prior) {
+    mubar <- sweep(mu[, -1], 1, mu[, 1], "+")/2
+    n <- nrow(x)
+    nclass <- length(prior)
+    nlambda <- dim(theta)[3]
+    score <- array(0, dim = c(n, nclass, nlambda))
+    for (k in 2:nclass) {
+        score[, k, ] <- sweep(x, 1, mubar[, k - 1], "-") %*% theta[k - 1, , ] + log(prior[k]) - 
+            log(prior[1])
+    }
+    pred <- apply(score, c(1, 3), which.max)
+    pred
+} 
