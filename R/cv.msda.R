cc50d0b4784604de19581ccc45158eb502d2d1d3
Yi Yang <emeryyi@gmail.com>
2014-09-28 03:14:56 -0500

    1. further improve the code

--- R/cv.msda.R
+++ R/cv.msda.R
@@ -1,36 +1,33 @@
-cv.msda <- function(x, y, nfolds = 5, foldid, lambda = NULL, lambda.opt = "min", ...) {
+cv.msda <- function(x, y, nfolds = 5, lambda = NULL, lambda.opt = "min", ...) {
     y <- drop(y)
-    n <- NROW(x)
-    p <- NCOL(x)
+    n <- nrow(x)
+    p <- ncol(x)
     K<-length(unique(y))
     prior<-rep(0,K)
     for(i in 1:K){prior[i]<-mean(y==i)}
     ### Fit the model once to get dimensions etc of output
     tmp <- msda(x, y, lambda = lambda, ...)
     lambda <- tmp$lambda
-    nlambda <- length(lambda)
     ### Now fit the nfold models and store them
-    if (missing(foldid)) foldid <- sample(rep(seq(nfolds), length = n)) else 			nfolds <- max(foldid)
+    foldid <- sample(rep(seq(nfolds), length = n))
     if (nfolds < 3) 
         stop("nfolds must be bigger than 3; nfolds=10 recommended")
     if (nfolds > n) 
         stop("The number of folds should be smaller than the sample size.")
-
-    nlams=double(nfolds)	
     residmat=matrix(NA,nfolds,length(lambda))
     good <- matrix(0, nfolds, length(lambda))
     for (i in seq(nfolds)) {
         which <- foldid == i
+		cat("\nFold ", i, " is running.\n")
         fitobj <- msda(x[!which, , drop = FALSE], y[!which], lambda = lambda)
       	preds <- predict(fitobj,x[which,,drop=FALSE])
        	nlami <- length(fitobj$lambda)
         residmat[i,seq(nlami)] <- colMeans(y[which] != preds)
         good[i, seq(nlami)] <- 1
     }
-    real_N <- apply(good, 2, sum)
-    # residmat[is.na(residmat)] <- 0.5
-    cv <- apply(residmat, 2, mean, na.rm = TRUE)
-    cv.error <- sqrt(apply(scale(residmat, cv, FALSE)^2, 2, mean, na.rm = TRUE)/(real_N - 1))
+    rN <- colSums(good)
+    cv <- colMeans(residmat, na.rm = TRUE)
+    cv.error <- sqrt(colMeans(scale(residmat, cv, FALSE)^2, na.rm = TRUE)/(rN - 1))
     if (lambda.opt == "min") {
         bestlambda <- min(lambda[which(cv == min(cv,na.rm=TRUE))])
     } else {
@@ -45,4 +42,3 @@ cv.msda <- function(x, y, nfolds = 5, foldid, lambda = NULL, lambda.opt = "min",
     class(obj) <- "cv.msda"
     obj
 } 
-
